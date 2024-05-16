19051dd69c1373d2fb983355dcfde6dd21d8fc40
Siyi Xu <e1300538@u.nus.edu>
2024-05-16 21:32:50 +0800

    update frontend UI/UX

---
+++ Frontend/Orbital/Views/UserProfile/MyIsland.swift
@@ -0,0 +1,18 @@
+//
+//  MyIsland.swift
+//  Orbital
+//
+//  Created by Xu Siyi on 16/5/24.
+//
+
+import SwiftUI
+
+struct MyIsland: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyIsland()
+}
