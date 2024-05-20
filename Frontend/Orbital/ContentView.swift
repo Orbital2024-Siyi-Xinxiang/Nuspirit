b1ec3f319dd9910850eac00d849017a04a5e8cff
Siyi Xu <e1300538@u.nus.edu>
2024-05-20 20:39:29 +0800

    update furniture-market view

--- Frontend/Orbital/ContentView.swift
+++ Frontend/Orbital/ContentView.swift
@@ -5,14 +5,16 @@ struct ContentView: View {
         NavigationView {
             ZStack() {
                 ZStack() {
-                    Rectangle()
-                        .foregroundColor(.clear)
-                        .frame(width: 283, height: 60)
-                        .background(Color(red: 0.61, green: 0.86, blue: 0.94).opacity(0.33))
-                        .cornerRadius(20)
-                        .offset(x: -0.50, y: -0.50)
-                    
                     NavigationLink(destination: RegisterView()) {
+                        
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 283, height: 60)
+                            .background(Color(red: 0.61, green: 0.86, blue: 0.94).opacity(0.33))
+                            .cornerRadius(20)
+                            .offset(x: -0.50, y: -0.50)
+                        
+                        
                         Text("Sign In")
                             .font(Font.custom("Press Start 2P", size: 20))
                             .foregroundColor(Color(red: 0.23, green: 0.23, blue: 0.23))
@@ -23,13 +25,15 @@ struct ContentView: View {
                 .offset(x: -9.50, y: 20.50)
                 
                 ZStack() {
-                    Rectangle()
-                        .foregroundColor(.clear)
-                        .frame(width: 283, height: 60)
-                        .background(Color(red: 0.61, green: 0.94, blue: 0.74).opacity(0.40))
-                        .cornerRadius(20)
-                        .offset(x: -0.50, y: -0.50)
                     NavigationLink(destination: SignInView()) {
+                        
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 283, height: 60)
+                            .background(Color(red: 0.61, green: 0.94, blue: 0.74).opacity(0.40))
+                            .cornerRadius(20)
+                            .offset(x: -0.50, y: -0.50)
+                        
                         Text("Sign Up")
                             .font(Font.custom("Press Start 2P", size: 20))
                             .foregroundColor(Color(red: 0.23, green: 0.23, blue: 0.23))
