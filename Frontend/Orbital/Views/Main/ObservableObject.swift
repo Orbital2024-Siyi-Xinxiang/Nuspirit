5aecdc191a7435f2591c46bf466ddf4d79f95ba1
Siyi Xu <e1300538@u.nus.edu>
2024-05-22 00:03:44 +0800

    update backend server.js
    add firebase configuration (but firebase console not connected yet)

---
+++ Frontend/Orbital/Views/Main/ObservableObject.swift
@@ -0,0 +1,18 @@
+//
+//  ObservableObject.swift
+//  Orbital
+//
+//  Created by Xu Siyi on 22/5/24.
+//
+
+import SwiftUI
+
+struct ObservableObject: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ObservableObject()
+}
