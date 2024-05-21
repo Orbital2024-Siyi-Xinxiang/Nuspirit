6b0ed7294045983ddf6947871e53c11542d6a171
Siyi Xu <e1300538@u.nus.edu>
2024-05-22 00:32:25 +0800

    update service

--- Frontend/Orbital/Services/FirebaseStorage.swift
+++ Frontend/Orbital/Services/FirebaseStorage.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import FirebaseFirestore
+
+class FirestoreService {
+    static let shared = FirestoreService()
+    let db = Firestore.firestore()
+
+    func addUser(uid: String, email: String, username: String) {
+        db.collection("users").document(uid).setData([
+            "email": email,
+            "username": username,
+            "createdAt": Timestamp()
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added with ID: \(uid)")
+            }
+        }
+    }
+}
