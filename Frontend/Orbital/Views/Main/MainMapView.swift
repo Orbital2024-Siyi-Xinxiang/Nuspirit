753105c1468a6bc63a87117aa2e3a8973830d732
Siyi Xu <e1300538@u.nus.edu>
2024-05-24 04:27:06 +0800

    update sign out functionality

--- Frontend/Orbital/Views/Main/MainMapView.swift
+++ Frontend/Orbital/Views/Main/MainMapView.swift
@@ -6,7 +6,7 @@ struct MainMapView: View {
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194),
                                                    span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
     @Binding var showSettingsOverlay: Bool
-
+    
     var body: some View {
         ZStack {
             Map(coordinateRegion: $region, showsUserLocation: true)
@@ -15,28 +15,29 @@ struct MainMapView: View {
                         region.center = userLocation.coordinate
                     }
                 }
-
+            
             if showSettingsOverlay {
                 SettingsOverlay(showSettingsOverlay: $showSettingsOverlay)
+                
             }
         }
     }
-}
-
-class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
-    private let locationManager = CLLocationManager()
-    @Published var userLocation: CLLocation?
-
-    override init() {
-        super.init()
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.startUpdatingLocation()
-    }
-
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else { return }
-        userLocation = location
+    
+    class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+        private let locationManager = CLLocationManager()
+        @Published var userLocation: CLLocation?
+        
+        override init() {
+            super.init()
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.startUpdatingLocation()
+        }
+        
+        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            guard let location = locations.last else { return }
+            userLocation = location
+        }
     }
 }
