278cb73ca393e9e01dbd1a1cc4b170b0ba5ccb35
Xinxiang0257 <houxinxiang0307009@gmail.com>
2024-05-24 11:47:18 +0800

    added new LocationService file to frontend

--- Frontend/Orbital/Services/LocationService.swift
+++ Frontend/Orbital/Services/LocationService.swift
@@ -1,32 +1,39 @@
 //
-//  LocationManager.swift
+//  LocationService.swift
 //  Orbital
 //
-//  Created by Xu Siyi on 20/5/24.
+//  Created by Wulala's Macbook on 14/5/24.
 //
 
 import Foundation
 import CoreLocation
 
-class LocationService: NSObject, ObservableObject, CLLocationManagerDelegate {
+struct IdentifiableLocation: Identifiable {
+    let id = UUID()
+    let location: CLLocation
+}
+
+class LocationService: NSObject, CLLocationManagerDelegate, ObservableObject {
+    static let shared = LocationService()
     private let locationManager = CLLocationManager()
-    @Published var location: CLLocation?
     
+    @Published var location: IdentifiableLocation? = nil
+
     override init() {
         super.init()
         locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestWhenInUseAuthorization() // Request permission to use location services
-        locationManager.startUpdatingLocation()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()  // Start location updates here
     }
-    
+
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let newLocation = locations.last else { return }
-        location = newLocation
+        guard let location = locations.last else { return }
+        self.location = IdentifiableLocation(location: location)
+        // Post location to the app
+        NotificationCenter.default.post(name: NSNotification.Name("LocationUpdate"), object: nil, userInfo: ["location": location])
     }
     
-    func locationService(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print("Failed to find user's location: \(error.localizedDescription)")
-    }
+    
 }
 
