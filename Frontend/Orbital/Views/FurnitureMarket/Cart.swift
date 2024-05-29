f48a3e21b9bcb7718dc2ec9cbd1afad4ca99b1d4
Siyi Xu <e1300538@u.nus.edu>
2024-05-29 11:37:17 +0800

    update unity utilities

--- Frontend/Orbital/Views/FurnitureMarket/Cart.swift
+++ Frontend/Orbital/Views/FurnitureMarket/Cart.swift
@@ -1,72 +1,18 @@
 import SwiftUI
 
 struct CartView: View {
-    @State private var items = [Item]() // Define items array
-
     var body: some View {
         VStack {
-            Button(action: {
-                print("Select All Button Tapped")
-            }) {
-                Text("Select All")
-                    .frame(width: 100, height: 50)
-                    .foregroundColor(.black)
-            }
-            
-            Button(action: {
-                print("Delete Button Tapped")
-            }) {
-                Text("Delete")
-                    .frame(width: 100, height: 50)
-                    .foregroundColor(.black)
-            }
-            
-            Spacer()
-            
-            Button(action: {
-                print("Checkout Button Tapped")
-            }) {
-                Text("Checkout")
-                    .frame(width: 100, height: 50)
-                    .foregroundColor(.black)
+            Text("Cart")
+                .font(.largeTitle)
+            // List of cart items
+            List {
+                // Populate with cart items
+                Text("Cart Item 1")
+                Text("Cart Item 2")
+                Text("Cart Item 3")
             }
         }
-        .padding()
-        
-        List {
-            ForEach(items) { item in
-                NavigationLink(destination: Text("Item at \(item.timestamp)")) {
-                    Text("Item at \(item.timestamp)")
-                }
-            }
-            .onDelete(perform: deleteItems)
-        }
-        .toolbar {
-            ToolbarItem(placement: .navigationBarTrailing) {
-                EditButton()
-            }
-            ToolbarItem {
-                Button(action: addItem) {
-                    Label("Add Item", systemImage: "plus")
-                }
-            }
-        }
-    }
-    
-    // Define Item struct
-    struct Item: Identifiable {
-        let id = UUID()
-        let timestamp = Date()
-    }
-    
-    // Define deleteItems function
-    func deleteItems(at offsets: IndexSet) {
-        items.remove(atOffsets: offsets)
-    }
-    
-    // Define addItem function
-    func addItem() {
-        items.append(Item())
     }
 }
 
