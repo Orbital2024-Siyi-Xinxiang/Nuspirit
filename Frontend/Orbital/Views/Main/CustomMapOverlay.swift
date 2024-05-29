f53bbf51dd05c23d3323c24ae78d29d1dbaa7669
Siyi Xu <e1300538@u.nus.edu>
2024-05-30 00:54:14 +0800

    update building view

--- Frontend/Orbital/Views/Main/CustomMapOverlay.swift
+++ Frontend/Orbital/Views/Main/CustomMapOverlay.swift
@@ -1,5 +1,7 @@
 import MapKit
+import SwiftUI
 
+<<<<<<< Updated upstream
 class CustomMapOverlay: NSObject, MKOverlay {
     var coordinate: CLLocationCoordinate2D
     var boundingMapRect: MKMapRect
@@ -7,27 +9,39 @@ class CustomMapOverlay: NSObject, MKOverlay {
     init(coordinate: CLLocationCoordinate2D, boundingMapRect: MKMapRect) {
         self.coordinate = coordinate
         self.boundingMapRect = boundingMapRect
+=======
+class CustomMapOverlay: NSObject, MKAnnotation {
+    var coordinate: CLLocationCoordinate2D {
+        return coordinate1
+    }
+    var coordinate1: CLLocationCoordinate2D
+    var coordinate2: CLLocationCoordinate2D
+    var title: String?
+    var levels: Int
+    var capacity: Int
+    var buildingID: String
+
+    init(coordinate1: CLLocationCoordinate2D, coordinate2: CLLocationCoordinate2D, title: String, levels: Int, capacity: Int, buildingID: String) {
+        self.coordinate1 = coordinate1
+        self.coordinate2 = coordinate2
+        self.title = title
+        self.levels = levels
+        self.capacity = capacity
+        self.buildingID = buildingID
+>>>>>>> Stashed changes
     }
 }
 
 class CustomMapOverlayRenderer: MKOverlayRenderer {
     override func draw(_ mapRect: MKMapRect, zoomScale: MKZoomScale, in context: CGContext) {
-        guard let image = UIImage(named: "TestMap") else { return }
+        guard let overlay = overlay as? CustomMapOverlay else { return }
+        
+        let rect1 = self.rect(for: MKMapPoint(overlay.coordinate1))
+        let rect2 = self.rect(for: MKMapPoint(overlay.coordinate2))
+        
+        let rect = CGRect(x: min(rect1.origin.x, rect2.origin.x), y: min(rect1.origin.y, rect2.origin.y), width: abs(rect1.origin.x - rect2.origin.x), height: abs(rect1.origin.y - rect2.origin.y))
 
-        let rect = self.rect(for: overlay.boundingMapRect)
-
-        // Save the context state
-        context.saveGState()
-
-        // Flip the context vertically
-        context.translateBy(x: rect.midX, y: rect.midY)
-        context.scaleBy(x: 1.0, y: -1.0)
-        context.translateBy(x: -rect.midX, y: -rect.midY)
-
-        // Draw the image
-        context.draw(image.cgImage!, in: rect)
-
-        // Restore the context state
-        context.restoreGState()
+        context.setFillColor(UIColor.red.withAlphaComponent(0.5).cgColor)
+        context.fill(rect)
     }
 }
