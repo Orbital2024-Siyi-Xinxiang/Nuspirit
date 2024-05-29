df696a6d2d8b0dc6c82a7125dcf3b3adcff6832b
Siyi Xu <e1300538@u.nus.edu>
2024-05-29 20:49:35 +0800

    obtain buildings json data

--- ThirdParty/parser.py
+++ ThirdParty/parser.py
@@ -2,30 +2,24 @@ from bs4 import BeautifulSoup
 import json
 
 # Load the HTML file
-with open('/Users/emma/Desktop/Repos/Orbital2024/Monorepo/Nuspirit/ThirdParty/nus_interactive_map.html', 'r') as file:
-    html_content = file.read()
+with open('/Users/emma/Desktop/Repos/Orbital2024/Monorepo/Nuspirit/ThirdParty/buildings_raw_data.json', 'r') as file:
 
-# Parse the HTML content using BeautifulSoup
-soup = BeautifulSoup(html_content, 'html.parser')
+    geojson_data = json.load(file)
 
-# Extract the GeoJSON data
-script_tags = soup.find_all('script')
-geojson_data = None
-
-for script in script_tags:
-    if 'geo_json_f064cd4f875a46ff469c8a29919046ff_add' in script.text:
-        start_index = script.text.find('geo_json_f064cd4f875a46ff469c8a29919046ff_add') + len('geo_json_f064cd4f875a46ff469c8a29919046ff_add(')
-        end_index = script.text.find(');', start_index)
-        geojson_str = script.text[start_index:end_index].strip()
-        if geojson_str:  # Check if the string is not empty
-            try:
-                geojson_data = json.loads(geojson_str)
-            except json.decoder.JSONDecodeError as e:
-                print(f"Failed to decode JSON: {e}")
-                continue
-        else:
-            print("geojson_str is empty")
-        break
+# for script in script_tags:
+#     if 'geo_json_f064cd4f875a46ff469c8a29919046ff_add' in script.text:
+#         start_index = script.text.find('geo_json_f064cd4f875a46ff469c8a29919046ff_add') + len('geo_json_f064cd4f875a46ff469c8a29919046ff_add(')
+#         end_index = script.text.find(');', start_index)
+#         geojson_str = script.text[start_index:end_index].strip()
+#         if geojson_str:  # Check if the string is not empty
+#             try:
+#                 geojson_data = json.loads(geojson_str)
+#             except json.decoder.JSONDecodeError as e:
+#                 print(f"Failed to decode JSON: {e}")
+#                 continue
+#         else:
+#             print("geojson_str is empty")
+#         break
 
 # Extract coordinates from the GeoJSON data
 buildings = []
@@ -35,9 +29,9 @@ if geojson_data:
         if feature['geometry']['type'] == 'Point':
             continue
         else:
-            coordinates1 = feature['bbox']['coordinates'][0]  # Assuming the first set of coordinates for simplicity
-            coordinates2 = feature['bbox']['coordinates'][1]
-            id = feature.get('id', None)[9:-1] # Extract the building ID
+            coordinates1 = [feature['bbox'][0], feature['bbox'][1]]
+            coordinates2 = [feature['bbox'][2], feature['bbox'][3]]
+            id = feature.get('id', None)[8:-1] # Extract the building ID
             buildings.append({
                 'name': id,
                 'coordinates': [coordinates1, coordinates2]
@@ -50,5 +44,5 @@ for building in buildings:
     print(f"Coordinates: {building['coordinates']}\n")
 
 # Optionally, save to a JSON file
-with open('buildings_data.json', 'w') as json_file:
+with open('/Users/emma/Desktop/Repos/Orbital2024/Monorepo/Nuspirit/ThirdParty/buildings_data.json', 'w') as json_file:
     json.dump(buildings, json_file, indent=4)
