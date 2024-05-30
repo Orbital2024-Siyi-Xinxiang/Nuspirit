d9c1f9a92713a8f01b794a2620e5deae975644fe
Siyi Xu <e1300538@u.nus.edu>
2024-05-30 11:41:29 +0800

    update spiritual island grid placement system

--- Nuspirit/Assets/Scripts/SpiritualIsland/PlayerControl/InputManager.cs
+++ Nuspirit/Assets/Scripts/SpiritualIsland/PlayerControl/InputManager.cs
@@ -2,17 +2,32 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    public delegate void OnTouchDelegate(Vector2 position);
+    public delegate void OnTouchDelegate(Vector2 position, bool isDragging);
     public static event OnTouchDelegate OnTouch;
+
+    private bool isDragging;
 
     void Update()
     {
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-            if (touch.phase == TouchPhase.Began)
+            Vector2 touchPosition = touch.position;
+
+            switch (touch.phase)
             {
-                OnTouch?.Invoke(touch.position);
+                case TouchPhase.Began:
+                    isDragging = false;
+                    OnTouch?.Invoke(touchPosition, isDragging);
+                    break;
+                case TouchPhase.Moved:
+                    isDragging = true;
+                    OnTouch?.Invoke(touchPosition, isDragging);
+                    break;
+                case TouchPhase.Ended:
+                    isDragging = false;
+                    OnTouch?.Invoke(touchPosition, isDragging);
+                    break;
             }
         }
     }
