1bb301f89c7e5d7e62c1e18c59bcec112cfed192
Siyi Xu <e1300538@u.nus.edu>
2024-05-31 02:22:47 +0800

    update unity settings

--- UnityIntegration/Assets/Scripts/SpiritualIsland/GridPlacement/CategoryDatabaseSO.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "CategoryDatabase", menuName = "ScriptableObjects/CategoryDatabase")]
-public class CategoryDatabaseSO : ScriptableObject
-{
-    public string[] categories;
-}
--- UnityIntegration/Assets/Scripts/SpiritualIsland/GridPlacement/IBuildingState.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public interface IBuildingState
-{
-    void OnEnterState();
-    void OnUpdate();
-    void OnExitState();
-}
