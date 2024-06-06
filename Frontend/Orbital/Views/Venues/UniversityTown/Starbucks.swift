fca1672b361c95220440b80d16fe5c47946f2382
Siyi Xu <e1300538@u.nus.edu>
2024-06-06 22:12:16 +0800

    update user credential settings into database

---
+++ Frontend/Orbital/Views/Venues/UniversityTown/Starbucks.swift
@@ -0,0 +1,18 @@
+//
+//  Starbucks.swift
+//  Orbital
+//
+//  Created by Xu Siyi on 6/6/24.
+//
+
+import SwiftUI
+
+struct Starbucks: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Starbucks()
+}
