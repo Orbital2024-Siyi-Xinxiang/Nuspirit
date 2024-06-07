ae04756437b65c03dc64704509db4d397c756f02
Siyi Xu <e1300538@u.nus.edu>
2024-06-07 20:44:45 +0800

    update user onboarding

--- Frontend/Orbital/Views/Launch/UserData.swift
+++ Frontend/Orbital/Views/Launch/UserData.swift
@@ -10,34 +10,34 @@ let UserData: [UserInfo] = [
         title: "Welcome!",
         headline: "How to address you?",
         image: "spirit-with-sunglasses-logo",
-        gradientColors: [Color.blue, Color.red]
+        gradientColors: [Color.blue, Color.purple, Color.red]
     ),
     UserInfo(
         data: "isStudentOrNot",
         title: "Basic Information",
         headline: "Are you currently an NUS student?",
         image: "spirit-with-sunglasses-logo",
-        gradientColors: [Color.blue, Color.red]
+        gradientColors: [Color.blue, Color.green, Color.yellow]
     ),
     UserInfo(
         data: "levelOfStudy",
         title: "Level of Study",
         headline: "What's your level of study?",
         image: "spirit-with-sunglasses-logo",
-        gradientColors: [Color.blue, Color.red]
+        gradientColors: [Color.pink, Color.red, Color.orange]
     ),
     UserInfo(
         data: "faculty",
         title: "Basic Information",
         headline: "Select your department/faculty",
         image: "spirit-with-sunglasses-logo",
-        gradientColors: [Color.blue, Color.red]
+        gradientColors: [Color.brown, Color.yellow, Color.orange]
     ),
     UserInfo(
         data: "major",
         title: "Basic Information",
         headline: "What's your major?",
         image: "spirit-with-sunglasses-logo",
-        gradientColors: [Color.blue, Color.red]
+        gradientColors: [Color.blue, Color.teal, Color.indigo]
     )
 ]
