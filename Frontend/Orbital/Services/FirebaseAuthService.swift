96f2c3c409ea177bdaf130938bd15a0107d034b3
Siyi Xu <e1300538@u.nus.edu>
2024-06-13 15:06:47 +0800

    update firebase messaging service

--- Frontend/Orbital/Services/FirebaseAuthService.swift
+++ Frontend/Orbital/Services/FirebaseAuthService.swift
@@ -16,5 +16,41 @@ struct FirebaseAuthService {
             return ""
         }
     }
+
+    func getCurrentEmail() -> String {
+        // Get the currently signed-in user
+        if let user = Auth.auth().currentUser {
+            // The user is signed in, retrieve the email
+            let email = user.email
+            return email ?? ""
+        } else {
+            return ""
+        }
+    }
+
+    func getCurrentDisplayName() -> String {
+        // Get the currently signed-in user
+        if let user = Auth.auth().currentUser {
+            // The user is signed in, retrieve the display name
+            let displayName = user.displayName
+            return displayName ?? ""
+        } else {
+            return ""
+        }
+    }
+
+    func getCurrentPhotoURL() -> URL {
+        // Get the currently signed-in user
+        if let user = Auth.auth().currentUser {
+            // The user is signed in, retrieve the photo URL
+            let photoURL = user.photoURL
+            return photoURL ?? URL(string: "")!
+        } else {
+            return URL(string: "")!
+        }
+    }
+
+
+    
     
 }
