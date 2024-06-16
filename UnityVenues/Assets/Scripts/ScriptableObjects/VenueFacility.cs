e0bd37ae957fad9e9d2f116c4b32f83d38d0d287
Siyi Xu <e1300538@u.nus.edu>
2024-06-16 22:13:55 +0800

    update unity  integration

---
+++ UnityVenues/Assets/Scripts/ScriptableObjects/VenueFacility.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "VenueFacilitiesSO", menuName = "Scriptable Objects/VenueFacilitiesSO")]
+public class VenueFacilitiesSO : ScriptableObject
+{
+    
+}
---
+++ UnityVenues/Assets/Scripts/ScriptableObjects/VenueTransferPointsSO.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "VenueTransferPointsSO", menuName = "Scriptable Objects/VenueTransferPointsSO")]
+public class VenueTransferPointsSO : ScriptableObject
+{
+    
+}
