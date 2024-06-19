ee5ea4c459fccd968828e1b1c879c1e7a983ce9a
Siyi Xu <e1300538@u.nus.edu>
2024-06-20 03:29:53 +0800

    update firestore data retrieval

--- UnityVenues/Assets/Scripts/VenueManagement/VenueRenderer.cs
+++ UnityVenues/Assets/Scripts/VenueManagement/VenueRenderer.cs
@@ -7,12 +7,10 @@ public class VenueRenderer : MonoBehaviour
     public GameObject backgroundPrefab;
     public Camera mainCamera;
 
-
     public Venue venue;
 
     // Prefab with Tilemap component
     private Tilemap backgroundTilemap;
-
 
     public void Initialize(string venueId)
     {
@@ -23,38 +21,49 @@ public class VenueRenderer : MonoBehaviour
 
     public void InitializeBackground()
     {
-        // Instantiate the background prefab and get the Tilemap component
-        GameObject background = Instantiate(backgroundPrefab, new Vector3(-5, -5, 0), Quaternion.identity);
-        backgroundTilemap = background.GetComponent<Tilemap>();
+        // Create a new GameObject for the Grid
+        GameObject gridObject = new GameObject("TilemapGrid");
+        gridObject.transform.position = new Vector3(-5, -5, 0);
 
-        if(venue.venueBackground != null)
+        // Add a Grid component to the Grid GameObject
+        Grid grid = gridObject.AddComponent<Grid>();
+
+        // Create a new GameObject for the Tilemap and make it a child of the Grid
+        GameObject tilemapObject = new GameObject("BackgroundTilemap");
+        tilemapObject.transform.SetParent(gridObject.transform, false);
+
+        // Add a Tilemap component to the Tilemap GameObject
+        backgroundTilemap = tilemapObject.AddComponent<Tilemap>();
+        TilemapRenderer tilemapRenderer = tilemapObject.AddComponent<TilemapRenderer>();
+
+        if (venue.venueBackground != null)
         {
             // Render the tiles based on the current venue
             RenderTiles();
         }
-
     }
 
     public void RenderTiles()
     {
         VenueBackground venueBackground = venue.venueBackground;
 
-            if (venueBackground.tileSet != null && venueBackground.tileSet.tiles != null)
-                 for (int y = 0; y < 100; y++)
+        if (venueBackground.tileSet != null && venueBackground.tileSet.tiles != null)
+        {
+            for (int y = 0; y < 100; y++)
+            {
+                for (int x = 0; x < 100; x++)
                 {
-                    for (int x = 0; x < 100; x++)
+                    int index = x + y * 100;
+                    if (index < venueBackground.tileSet.tiles.Length)
                     {
-                        int index = x + y * 100;
-                        if (index < venueBackground.tileSet.tiles.Length)
-                        {
-                            TileBase tile = venueBackground.tileSet.tiles[index];
-                            backgroundTilemap.SetTile(new Vector3Int(x, y, 0), tile);
-                        }
+                        TileBase tile = venueBackground.tileSet.tiles[index];
+                        backgroundTilemap.SetTile(new Vector3Int(x, y, 0), tile);
                     }
                 }
-
-                // Complete the background object and assign it back to the scriptable object
-                venueBackground.background = backgroundTilemap;
             }
+
+            // Complete the background object and assign it back to the scriptable object
+            venueBackground.background = backgroundTilemap;
         }
-   
+    }
+}
--- UnityVenues/Assets/Scripts/VenueObjects/Facility.cs
+++ UnityVenues/Assets/Scripts/VenueObjects/Facility.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewMonoBehaviour : MonoBehaviour
+public class Facility : MonoBehaviour
 {
 	// Use this for initialization
 	void Start()
