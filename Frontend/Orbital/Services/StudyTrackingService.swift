3cb1766af87ad2d83855c8072af660e15103fa95
Siyi Xu <e1300538@u.nus.edu>
2024-06-27 17:43:08 +0800

    update swiftUI view

--- Frontend/Orbital/Services/StudyTrackingService.swift
+++ Frontend/Orbital/Services/StudyTrackingService.swift
@@ -24,21 +24,18 @@ struct StudyTrackingService {
         let date = Date()
         let calendar = Calendar.current
         let hour = calendar.component(.hour, from: date)
-        let minute = calendar.component(.minute, from: date)
         return hour
     }
     
     public func getCurrentMinInt() -> Int {
         let date = Date()
         let calendar = Calendar.current
-        let hour = calendar.component(.hour, from: date)
         let minute = calendar.component(.minute, from: date)
         return minute
     }
     
     public func updateStudyTime(numOfMin: Int, venueId: String) {
         let currentDate = getCurrentDateString()
-        let currentTime = getCurrentTimeInt()
         let authService = FirebaseAuthService()
         let db = Firestore.firestore()
         let currentUserId = authService.getCurrentUid()
@@ -51,37 +48,29 @@ struct StudyTrackingService {
         let userStatsRef = db.collection("users_statistics").document(currentUserId)
         
         userStatsRef.getDocument { (document, error) in
+            var studyTimeData = [String: Int]()
+            var studyVenuesData = [String: Int]()
+            
             if let document = document, document.exists {
-                
-                
-                if var data = document.data(), var mapData = data["studyTime"] as? [String: Int] {
-                    mapData["totalMinutes", default: 0] += numOfMin
-                    userStatsRef.updateData(["studyTime": mapData]) { err in
-                        if let err = err {
-                            print("Error updating document: \(err)")
-                        } else {
-                            print("Document successfully updated")
-                        }
-                    }
-                } else {
-                    userStatsRef.setData(["studyTime": ["totalMinutes": numOfMin]]) { err in
-                        if let err = err {
-                            print("Error setting document: \(err)")
-                        } else {
-                            print("Document successfully written")
-                        }
-                    }
+                if let data = document.data() {
+                    studyTimeData = data["study_time_data"] as? [String: Int] ?? [:]
+                    studyVenuesData = data["study_venues_data"] as? [String: Int] ?? [:]
                 }
-            } else {
-                print("Document does not exist")
-                
-                userStatsRef.setData(["study_time_data": [currentDate : numOfMin]]) {
-                    err in
-                    if let err = err {
-                        print("Error setting document: \(err)")
-                    } else {
-                     // do nothing
-                    }
+            }
+            
+            // Update study time data
+            studyTimeData[currentDate, default: 0] += numOfMin
+            // Update study venues data
+            studyVenuesData[venueId, default: 0] += numOfMin
+            
+            userStatsRef.updateData([
+                "study_time_data": studyTimeData,
+                "study_venues_data": studyVenuesData
+            ]) { err in
+                if let err = err {
+                    print("Error updating document: \(err)")
+                } else {
+                    print("Document successfully updated")
                 }
             }
         }
