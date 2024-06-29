98c1d3f9f61d9fe1ad12c681fa9f94126bee9467
Siyi Xu <e1300538@u.nus.edu>
2024-06-29 14:03:31 +0800

    update unity framework embedding

---
+++ Frontend/Orbital/Views/UserProfile/AvatarDesign.swift
@@ -0,0 +1,18 @@
+//
+//  AvatarDesign.swift
+//  Orbital
+//
+//  Created by Xu Siyi on 28/6/24.
+//
+
+import SwiftUI
+
+struct AvatarDesign: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AvatarDesign()
+}
