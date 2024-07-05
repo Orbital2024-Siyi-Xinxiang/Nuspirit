0a645b1ba1995c1110e6f7abc47a5e63fe4b6489
Siyi Xu <e1300538@u.nus.edu>
2024-07-06 02:00:05 +0800

    test podfile

---
+++ UnityScenes/iosBuild/OrbitalApp-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  OrbitalApp-Bridging-Header.h
+//  Unity-iPhone
+//
+//  Created by Xu Siyi on 6/7/24.
+//
+
+#import <CoreFoundation/CoreFoundation.h>
+#import <CoreGraphics/CoreGraphics.h>
+#import <dispatch/dispatch.h>
+#import <objc/NSObject.h>
+#import <os/object.h>
---
+++ UnityScenes/iosBuild/OrbitalApp/Views/LandingView.swift
@@ -0,0 +1,18 @@
+//
+//  LandingView.swift
+//  OrbitalApp
+//
+//  Created by Xu Siyi on 6/7/24.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandingView()
+}
