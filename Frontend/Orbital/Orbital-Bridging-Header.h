221dd3dba4890c985147ad185c0a229f935363e0
Siyi Xu <e1300538@u.nus.edu>
2024-07-06 00:39:08 +0800

    update two working version: orbital.xworkspace and unity-iphone.workspace

--- Frontend/Orbital/Orbital-Bridging-Header.h
+++ Frontend/Orbital/Orbital-Bridging-Header.h
@@ -1,6 +1,6 @@
 
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 
-#import <UnityFramework/NativeCallProxy.h>
-#import <UnityFramework/UnityFramework.h>
+//#import <UnityFramework/NativeCallProxy.h>
+//#import <UnityFramework/UnityFramework.h>
 
---
+++ UnityScenes/iosBuild/OrbitalApp/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  OrbitalApp
+//
+//  Created by Xu Siyi on 6/7/24.
+//
+
+import Foundation
+import FirebaseCore
+import FirebaseAuth
+import FirebaseAuthUI
+import UserNotifications
+import FirebaseGoogleAuthUI
+import FirebaseOAuthUI
+import FirebaseEmailAuthUI
+import FirebaseEmailAuthUI
+import UIKit
+import SwiftUI
+import GoogleSignIn
+import FBSDKCoreKit
+import FBSDKLoginKit
