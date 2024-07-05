e2d7cf3a2aaf16501f0edd9c1cca5d45db85b581
Siyi Xu <e1300538@u.nus.edu>
2024-07-05 19:38:18 +0800

    update unity build embedded swiftui ap

---
+++ Frontend/Unity/unity.h
@@ -0,0 +1,18 @@
+//
+//  unity.h
+//  unity
+//
+//  Created by Xu Siyi on 5/7/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for unity.
+FOUNDATION_EXPORT double unityVersionNumber;
+
+//! Project version string for unity.
+FOUNDATION_EXPORT const unsigned char unityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <unity/PublicHeader.h>
+
+
---
+++ UnityScenes/iosBuild/OrbitalApp/OrbitalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrbitalAppApp.swift
+//  OrbitalApp
+//
+//  Created by Xu Siyi on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct OrbitalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
