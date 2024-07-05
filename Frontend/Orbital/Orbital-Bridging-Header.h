255dae668fe66667d2276bad07be7203b4af6bc4
Siyi Xu <e1300538@u.nus.edu>
2024-07-05 20:33:10 +0800

    update package dependencies

--- Frontend/Orbital/Orbital-Bridging-Header.h
+++ Frontend/Orbital/Orbital-Bridging-Header.h
@@ -1,6 +1,6 @@
-//
-////  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-//#import <Unity/UnityFramework/NativeCallProxy.h>
-//#import <Unity/UnityFramework/UnityFramework.h>
-//
+
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+
+#import <UnityFramework/NativeCallProxy.h>
+#import <UnityFramework/UnityFramework.h>
+
