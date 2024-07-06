15e90e8bd27bb4db5feb3fd61e33ade790da38ee
Siyi Xu <e1300538@u.nus.edu>
2024-07-07 04:13:13 +0800

    update full app

---
+++ FullApp/Nuspirit/Nuspirit/NuspiritApp.swift
@@ -0,0 +1,17 @@
+//
+//  NuspiritApp.swift
+//  Nuspirit
+//
+//  Created by Xu Siyi on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct NuspiritApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
